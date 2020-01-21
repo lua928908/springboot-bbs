23bd353a98287c11c828202b03161ac74e0568cd
lua928908 <47199153+lua928908@users.noreply.github.com>
2020-01-22 00:09:10 +0900

    머스테치 오류 수정

--- src/main/java/com/study/springboot/springbootbbs/MyController.java
+++ src/main/java/com/study/springboot/springbootbbs/MyController.java
@@ -1,19 +1,29 @@
 package com.study.springboot.springbootbbs;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MyController {
     @RequestMapping("/")
-    public @ResponseBody // @ResponseBody 어노테이션때문에 단순 스트링을 리턴?
-    String root() throws Exception{
-        return "JSP in Gradle";
-    }
+    public String root(Model model) throws Exception{
 
-    @RequestMapping("/test")
-    public String test(){
-        return "test";
+        List<String> list = new ArrayList<>();
+
+        list.add("test01");
+        list.add("test02");
+        list.add("test03");
+
+        model.addAttribute("lists", list);
+        model.addAttribute("ObjectTest", "테스트입니다.");
+        model.addAttribute("name", "홍길동");
+
+        return "index";
     }
 }
