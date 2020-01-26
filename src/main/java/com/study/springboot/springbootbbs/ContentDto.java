04cc72ef001ab8a2acb1496231688dec731fa978
lua928908 <47199153+lua928908@users.noreply.github.com>
2020-01-26 17:42:19 +0900

    ContentValidator 대신 @Valid 사용하기
    꿀이네

--- src/main/java/com/study/springboot/springbootbbs/ContentDto.java
+++ src/main/java/com/study/springboot/springbootbbs/ContentDto.java
@@ -2,9 +2,18 @@ package com.study.springboot.springbootbbs;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class ContentDto {
     private int id;
+    @NotNull(message="writer is null.") //  null일 수 없다.
+    @NotEmpty(message="writer is empty.") // empty일 수 없다.
+    @Size(min=3, max=10, message="writer min3, max10.") // 최소 3, 최대10자리
     private String writer;
+    @NotNull(message="content is null.")
+    @NotEmpty(message="content is empty.")
     private String content;
 }
--- src/main/java/com/study/springboot/springbootbbs/MyController.java
+++ src/main/java/com/study/springboot/springbootbbs/MyController.java
@@ -3,9 +3,12 @@ package com.study.springboot.springbootbbs;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,13 +27,30 @@ public class MyController {
     }
 
     @RequestMapping("/create")
-    public String test1(@ModelAttribute("dto") ContentDto contentDto, BindingResult result){ // contentDto라는 값을 Modal에 dto라는 key이름으로 저장하는 의미
+    public String test1(@ModelAttribute("dto") @Valid ContentDto contentDto, BindingResult result){
+        /*
+            @ModelAttribute("dto") -> contentDto를 Model에 dto라는 key로 넣어주겠다는 의미.
+            @Valid -> 파라미터가 들어올때 contentDto를 항상 검증하겠다는 의미.
+            BindingResult result -> 유효성 검증을한 contentDto에 에러가 있다면 result에 에러가 담김.
+        */
         String page = "done";
         System.out.println(contentDto);
 
-        ContentValidator validator = new ContentValidator();
-        validator.validate(contentDto, result); // validate메서드를 통해 contentDto를 검사하고 에러가있으면 result에 담음
         if(result.hasErrors()){
+//            if(result.getFieldError("writer") != null){
+//                System.out.println("1:"+result.getFieldError("writer").getCode());
+//            }
+//            if(result.getFieldError("content") != null){
+//                System.out.println("2:"+result.getFieldError("content").getCode());
+//            }
+
+            if(result.getFieldError("writer") != null){
+                System.out.println("1:"+result.getFieldError("writer").getDefaultMessage());
+            }
+            if(result.getFieldError("content") != null){
+                System.out.println("2:"+result.getFieldError("content").getDefaultMessage());
+            }
+
             page = "create";
         }
 
