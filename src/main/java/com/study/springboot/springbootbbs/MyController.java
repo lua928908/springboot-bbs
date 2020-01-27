b7414fef0e0f4ad35880b7ce0564e3f2cce2156c
lua928908 <47199153+lua928908@users.noreply.github.com>
2020-01-27 12:47:56 +0900

    JDBC connect 테스트

--- src/main/java/com/study/springboot/springbootbbs/MyController.java
+++ src/main/java/com/study/springboot/springbootbbs/MyController.java
@@ -1,12 +1,10 @@
 package com.study.springboot.springbootbbs;
 
+import com.study.springboot.springbootbbs.jdbc.MyUserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -14,51 +12,18 @@ import java.util.List;
 
 @Controller
 public class MyController {
+    @Autowired
+    private MyUserDAO userDao;
+
     @RequestMapping("/")
-    public String root(Model model) throws Exception{
-        List<String> list = new ArrayList<>();
-        list.add("test01");
-        list.add("test02");
-        list.add("test03");
-        model.addAttribute("lists", list);
-        model.addAttribute("ObjectTest", "테스트입니다.");
-        model.addAttribute("name", "홍길동");
-        return "index";
+    public @ResponseBody String root() throws Exception{
+        return "JDBC Templete 사용하기";
     }
 
-    @RequestMapping("/create")
-    public String test1(@ModelAttribute("dto") @Valid ContentDto contentDto, BindingResult result){
-        /*
-            @ModelAttribute("dto") -> contentDto를 Model에 dto라는 key로 넣어주겠다는 의미.
-            @Valid -> 파라미터가 들어올때 contentDto를 항상 검증하겠다는 의미.
-            BindingResult result -> 유효성 검증을한 contentDto에 에러가 있다면 result에 에러가 담김.
-        */
-        String page = "done";
-        System.out.println(contentDto);
-
-        if(result.hasErrors()){
-//            if(result.getFieldError("writer") != null){
-//                System.out.println("1:"+result.getFieldError("writer").getCode());
-//            }
-//            if(result.getFieldError("content") != null){
-//                System.out.println("2:"+result.getFieldError("content").getCode());
-//            }
-
-            if(result.getFieldError("writer") != null){
-                System.out.println("1:"+result.getFieldError("writer").getDefaultMessage());
-            }
-            if(result.getFieldError("content") != null){
-                System.out.println("2:"+result.getFieldError("content").getDefaultMessage());
-            }
-
-            page = "create";
-        }
-
-        return page;
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String userlistPage(Model model){
+        model.addAttribute("users", userDao.list()); // userDao.list()를 호출하면 쿼리문의 실행결과를 Modal의 users로 담게된다.
+        return "userlist";
     }
 
-    @RequestMapping("/done")
-    public String test2(){
-        return "done";
-    }
 }
---
+++ src/main/java/com/study/springboot/springbootbbs/jdbc/MyUserDTO.java
@@ -0,0 +1,9 @@
+package com.study.springboot.springbootbbs.jdbc;
+
+import lombok.Data;
+
+@Data
+public class MyUserDTO {
+    private String id;
+    private String name;
+}
